731b2f8cb8fe53a7a98db75f2193ebc7012038cb
zhaoyang <zhaoyang01@ebnew.com>
2020-12-01 20:42:25 +0800

    作业补充提交

--- Week_01/battle/n11_盛最多水的容器.java
+++ Week_01/battle/n11_盛最多水的容器.java
@@ -1,57 +1,9 @@
-package org.example.week01;
+package battle;
 
+/**
+ * 实战题目 双指针
+ */
 public class n11_盛最多水的容器 {
-    /**
-     * 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i,
-     * ai) 和 (i, 0) 。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
-     * <p>
-     * 说明：你不能倾斜容器。
-     * <p>
-     * <p>
-     * <p>
-     * 示例 1：
-     * <p>
-     * <p>
-     * <p>
-     * <p>
-     * 输入：[1,8,6,2,5,4,8,3,7]
-     * 输出：49
-     * 解释：图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
-     * <p>
-     * 示例 2：
-     * <p>
-     * <p>
-     * 输入：height = [1,1]
-     * 输出：1
-     * <p>
-     * <p>
-     * 示例 3：
-     * <p>
-     * <p>
-     * 输入：height = [4,3,2,1,4]
-     * 输出：16
-     * <p>
-     * <p>
-     * 示例 4：
-     * <p>
-     * <p>
-     * 输入：height = [1,2,1]
-     * 输出：2
-     * <p>
-     * <p>
-     * <p>
-     * <p>
-     * 提示：
-     * <p>
-     * <p>
-     * n = height.length
-     * 2 <= n <= 3 * 104
-     * 0 <= height[i] <= 3 * 104
-     * <p>
-     * Related Topics 数组 双指针
-     * 👍 2010 👎 0
-     */
-
 
     //leetcode submit region begin(Prohibit modification and deletion)
     public static void main(String[] args) {
---
+++ Week_01/task/n66_加一.java
@@ -0,0 +1,38 @@
+package task;
+
+import java.util.Arrays;
+
+/**
+ * 【day 2】
+ */
+public class n66_加一{
+
+    public static void main(String[] args) {
+        int [] a = {1,2,3,4,5,6,7};
+
+        int [] b = {9,9,8,9,9,9,9};
+
+        System.out.println(Arrays.toString(plusOne(a)));
+        System.out.println(Arrays.toString(plusOne(b)));
+
+    }
+
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] == 9) {
+                digits[i] = 0;
+            } else {
+                digits[i]++;
+                return digits;
+            }
+        }
+
+        if (digits[0] == 0) {
+            int[] array = new int[digits.length + 1];
+            array[1] = 1;
+            return array;
+        }
+        return digits;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
